744ed660224e84f6307a1c35029cf57e41dd352d
Paco Martin Fernandez <erespia2@gmail.com>
2015-06-17 19:10:04 +0100

    MEAN Scaffolding with Yahoo Finance API and HighStock Charts

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,121 @@
+'use strict';
+
+angular.module('finsentaApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+    $scope.quote = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+    var companyID = 'CABK.MC';
+    var companyName = "Caixa Bank";
+
+    $http.get('/api/things/quote/'+ companyID).success(function(quotes) {
+
+        var stock = [];
+        var positiveActions = [];
+        var negativeActions = [];
+        var minimumStock = 99999;
+        for (var i in quotes){
+          stock.push([(new Date(quotes[i].date)).getTime(), quotes[i].close]);
+          positiveActions.push([(new Date(quotes[i].date)).getTime(), quotes[i].close/1.02]);
+          negativeActions.push([(new Date(quotes[i].date)).getTime(), quotes[i].close/1.07]);
+          if (minimumStock > quotes[i].close){
+            minimumStock = quotes[i].close;
+          }
+        }
+
+        Highcharts.setOptions({
+          global : {
+              useUTC : false
+          }
+        });
+
+        // Create the chart
+        $('#container').highcharts('StockChart', {
+            chart : {
+                events : {
+                    load : function () {
+
+                        // set up the updating of the chart each second
+                        /*var stockS = this.series[0];
+                        var pAS = this.series[1];
+                        var nAS = this.series[2];
+                        var i = 1;
+                        setInterval(function () {
+                            var x = (new Date()).getTime()+(86400000*i++), // current time
+                                y = Math.round(Math.random() * 5);
+                            stockS.addPoint([x, y], true, true);
+                            pAS.addPoint([x, y/1.05], true, true);
+                            nAS.addPoint([x, y/1.1], true, true);
+                            // TODO: Ajustar minimumStock si es necesario y actualizar yAxis.min
+                        }, 1000);*/
+                    }
+                }
+            },
+
+            rangeSelector : {
+                inputEnabled: false,
+                selected : 1
+            },
+
+            title : {
+                text : companyName + ' Stock'
+            },
+
+            xAxis: {
+              title: {
+                  text: 'Date'
+              }
+            },
+
+            yAxis: {
+                title: {
+                    text: 'Stock Cotization'
+                },
+                min: minimumStock
+            },
+
+            series : [{
+                        name : 'Close',
+                        data : stock,
+                        tooltip: {
+                            valueDecimals: 4
+                        }
+                      },
+                      {
+                        type: 'column',
+                        name: 'Positive Actions',
+                        data: positiveActions,
+                        color: '#5fc75f',
+                        tooltip: {
+                            valueDecimals: 0
+                        }
+                      },
+                      {
+                        type: 'column',
+                        name: 'Negative Actions',
+                        data: negativeActions,
+                        color: '#bf2600',
+                        tooltip: {
+                            valueDecimals: 0
+                        }
+                      }]
+        });
+      
+    });
+
+    $scope.addThing = function() {
+      if($scope.newThing === '') {
+        return;
+      }
+      $http.post('/api/things', { name: $scope.newThing });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    }; 
+  });
---
+++ server/api/thing/thing.controller.js
@@ -0,0 +1,96 @@
+/**
+ * Using Rails-like standard naming convention for endpoints.
+ * GET     /things              ->  index
+ * POST    /things              ->  create
+ * GET     /things/:id          ->  show
+ * PUT     /things/:id          ->  update
+ * DELETE  /things/:id          ->  destroy
+ */
+
+'use strict';
+
+var _ = require('lodash');
+var Thing = require('./thing.model');
+
+// Get list of things
+exports.index = function(req, res) {
+  Thing.find(function (err, things) {
+    if(err) { return handleError(res, err); }
+    return res.json(200, things);
+  });
+};
+
+// Get a single thing
+exports.show = function(req, res) {
+  Thing.findById(req.params.id, function (err, thing) {
+    if(err) { return handleError(res, err); }
+    if(!thing) { return res.send(404); }
+    return res.json(thing);
+  });
+};
+
+// Creates a new thing in the DB.
+exports.create = function(req, res) {
+  Thing.create(req.body, function(err, thing) {
+    if(err) { return handleError(res, err); }
+    return res.json(201, thing);
+  });
+};
+
+// Updates an existing thing in the DB.
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  Thing.findById(req.params.id, function (err, thing) {
+    if (err) { return handleError(res, err); }
+    if(!thing) { return res.send(404); }
+    var updated = _.merge(thing, req.body);
+    updated.save(function (err) {
+      if (err) { return handleError(res, err); }
+      return res.json(200, thing);
+    });
+  });
+};
+
+// Deletes a thing from the DB.
+exports.destroy = function(req, res) {
+  Thing.findById(req.params.id, function (err, thing) {
+    if(err) { return handleError(res, err); }
+    if(!thing) { return res.send(404); }
+    thing.remove(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.send(204);
+    });
+  });
+};
+
+function handleError(res, err) {
+  return res.send(500, err);
+}
+
+var yahooFinance = require('yahoo-finance');
+
+exports.quote = function(req, res) {
+  yahooFinance.historical({
+    symbol: req.params.id,
+    from: '2014-01-01',
+    to: formatDate(new Date()),
+    // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+  }, function (err, quotes) {
+    if(err) { return handleError(res, err); }
+
+    return res.json(200, quotes);
+  });
+    
+};
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
