8edc1f76283e598fd94688d00d39806f58f8ecb8
Jorge Carballo <jelcaf@gmail.com>
2015-07-12 15:17:32 +0100

    Add type to UserValue

--- server/api/sentiment/uservalue.model.js
+++ server/api/sentiment/uservalue.model.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var UserValueSchema = new Schema({
+  type: String, /* generic or not */
   value: { type : Schema.Types.ObjectId, ref : 'Value' },
   query: [{ type : Schema.Types.ObjectId, ref : 'Query' }],
   keyGroup: { type : Schema.Types.ObjectId, ref : 'KeyGroup' }
